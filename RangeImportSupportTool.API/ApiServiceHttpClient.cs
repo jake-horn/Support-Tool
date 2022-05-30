466c3ed5781e0fb1cadf77d154de1aea8b9e2d41
Jake <spawny08@hotmail.com>
2022-05-30 20:23:48 +0100

    initial commit

---
+++ RangeImportSupportTool.API/ApiServiceHttpClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace RangeImportSupportTool.APIService
+{
+    /* 
+     * Need to think about how we are going to populate the models and work out this class correctly
+     * It isn't going to work the same as the static class made in the console application
+     * we also need to sort out the api config and configuration manager set up
+     * */
+    public class ApiServiceHttpClient : HttpClient
+    {
+
+        public ApiServiceHttpClient()
+        {
+            this.BaseAddress = new Uri("https://nielsenbrandbank.freshservice.com/api/v2/");
+        }
+    }
+}
---
+++ RangeImportSupportTool.WPF/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RangeImportSupportTool.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RangeImportSupportTool.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IServiceProvider serviceProvider = CreateServiceProvider();
+
+            Window window = new MainWindow();
+
+            window.DataContext = serviceProvider.GetRequiredService<MainViewModel>();
+
+            window.Show();
+
+            base.OnStartup(e);
+        }
+
+        private IServiceProvider CreateServiceProvider()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddScoped<MainViewModel>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
